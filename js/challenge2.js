97e501cdea72dbaf18767a622523346286bb6b14
alyssrod <125409694+alyssrod@users.noreply.github.com>
2024-04-15 22:24:45 -0400

    Update challenge2.js

--- js/challenge2.js
+++ js/challenge2.js
@@ -1,29 +1,14 @@
 function challenge2(){
-
-   let bill = document.querySelector('#bill')
-
-   let home = document.querySelector('#home')
-
- 
-
-    console.log('this works')
-
- 
-
-    if(document.querySelector('#sameAddress').checked){
-
-        home.value=bill.value;
-
-        home.disabled=true;
-
+    let bill = document.querySelector('#bill');
+    let home = document.querySelector('#home');
+    if(bill && home){
+        console.log('this works');
+        if(document.querySelector('#sameAddress').checked){
+            home.value = bill.value;
+            home.disabled = true;
+        } else {
+            home.value = "";
+            home.disabled = false;
+        }
     }
-
-    else{
-
-        home.value = ""
-
-        home.disabled = false
-
-    }
-
 }
