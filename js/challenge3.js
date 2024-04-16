d4957a4d0982707a9c486e0026ddf295994b7eb2
alyssrod <125409694+alyssrod@users.noreply.github.com>
2024-04-15 22:25:04 -0400

    Update challenge3.js

--- js/challenge3.js
+++ js/challenge3.js
@@ -1,23 +1,8 @@
 function challenge3(){
-
-    var standing = null
-
-    var grad_date = null
-
- 
-
-    var grad_date = document.querySelector('input[name="gradDate"]:checked')
-
-    var standing = document.querySelector('input[name="standing"]:checked')
-
- 
-
+    var grad_date = document.querySelector('input[name="gradDate"]:checked');
+    var standing = document.querySelector('input[name="standing"]:checked');
     if (grad_date == null || standing == null){
-
-        alert("You must select a choice from each set of radio buttons")
-
-        return false
-
+        alert("You must select a choice from each set of radio buttons");
+        return false;
     }
-
 }
