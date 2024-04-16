9fc36ff28be6b3a1bb6267a43806b7a83c14279c
alyssrod <125409694+alyssrod@users.noreply.github.com>
2024-04-15 21:45:12 -0400

    Update challenge5.js

--- js/challenge5.js
+++ js/challenge5.js
@@ -1,1 +1,29 @@
+function challenge5(element){
 
+    console.log("Hovering")
+
+    console.log(element.src)
+
+    console.log(element.alt)
+
+ 
+
+    document.querySelector("#display").style.backgroundImage = "url('" + element.src + "')"
+
+    document.querySelector("#display").innerHTML = element.alt
+
+}
+
+ 
+
+function challenge5leave(element){
+
+ 
+
+    console.log("Leaving")
+
+        document.querySelector("#display").style.backgroundImage = ""
+
+        document.querySelector("#display").innerHTML = "Hover over an image below to display the image and the alt text."
+
+}
