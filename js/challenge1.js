9c138814b049ccdd02d45c5f3b6ae65ef454c526
alyssrod <125409694+alyssrod@users.noreply.github.com>
2024-04-15 21:40:54 -0400

    Update challenge1.js

--- js/challenge1.js
+++ js/challenge1.js
@@ -1,1 +1,15 @@
+ if(document.getElementById('subscribe').checked == true){
 
+        alert('hello world');
+
+        document.querySelector('#emailDiv').style.display="block";
+
+    }
+
+    else{
+
+        document.querySelector('#emailDiv').style.display="none";
+
+    }
+
+}
