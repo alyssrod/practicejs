41a02b6258541a39c4a67207a42b7a10e561a4fa
alyssrod <125409694+alyssrod@users.noreply.github.com>
2024-04-15 21:41:14 -0400

    Update challenge2.js

--- js/challenge2.js
+++ js/challenge2.js
@@ -1,1 +1,27 @@
+let bill = document.querySelector('#bill')
 
+   let home = document.querySelector('#home')
+
+ 
+
+    console.log('this works')
+
+ 
+
+    if(document.querySelector('#sameAddress').checked){
+
+        home.value=bill.value;
+
+        home.disabled=true;
+
+    }
+
+    else{
+
+        home.value = ""
+
+        home.disabled = false
+
+    }
+
+}
