78a0fb7e893d490865a93814df6bffea131460ab
alyssrod <125409694+alyssrod@users.noreply.github.com>
2024-04-15 22:25:47 -0400

    Update challenge5.js

--- js/challenge5.js
+++ js/challenge5.js
@@ -1,29 +1,19 @@
 function challenge5(element){
-
-    console.log("Hovering")
-
-    console.log(element.src)
-
-    console.log(element.alt)
-
- 
-
-    document.querySelector("#display").style.backgroundImage = "url('" + element.src + "')"
-
-    document.querySelector("#display").innerHTML = element.alt
-
+    var display = document.querySelector("#display");
+    if(display){
+        console.log("Hovering");
+        console.log(element.src);
+        console.log(element.alt);
+        display.style.backgroundImage = "url('" + element.src + "')";
+        display.innerHTML = element.alt;
+    }
 }
 
- 
-
 function challenge5leave(element){
-
- 
-
-    console.log("Leaving")
-
-        document.querySelector("#display").style.backgroundImage = ""
-
-        document.querySelector("#display").innerHTML = "Hover over an image below to display the image and the alt text."
-
+    var display = document.querySelector("#display");
+    if(display){
+        console.log("Leaving");
+        display.style.backgroundImage = "";
+        display.innerHTML = "Hover over an image below to display the image and the alt text.";
+    }
 }
